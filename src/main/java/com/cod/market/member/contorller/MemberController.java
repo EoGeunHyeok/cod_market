dca62f7e37fa40be12f86990595cb5f9c65090fe
EoGeunHyeok <eghsamo@naver.com>
2024-07-09 14:12:15 +0900

    login & signup controller

--- src/main/java/com/cod/market/member/contorller/MemberController.java
+++ src/main/java/com/cod/market/member/contorller/MemberController.java
@@ -1,11 +1,22 @@
 package com.cod.market.member.contorller;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@Getter
-@Setter
+@RequestMapping("/member")
 public class MemberController {
+    @GetMapping("/login")
+    @ResponseBody
+    public String login() {
+        return "로그인 페이지 입니다.";
+    }
+
+    @GetMapping("/signup")
+    @ResponseBody
+    public String signup() {
+        return "회원가입 페이지 입니다.";
+    }
 }
