23c5dcba0a34e0d30a224cce5b1f0df9c700c1a0
EoGeunHyeok <eghsamo@naver.com>
2024-07-09 16:14:42 +0900

    signup form

--- src/main/java/com/cod/market/member/contorller/MemberController.java
+++ src/main/java/com/cod/market/member/contorller/MemberController.java
@@ -1,19 +1,34 @@
 package com.cod.market.member.contorller;
 
+import com.cod.market.member.form.MemberForm;
+import com.cod.market.member.service.MemberService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/member")
+@RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
+
     @GetMapping("/login")
     public String login() {
         return "member/login";
     }
 
     @GetMapping("/signup")
-    public String signup() {
+    public String showSignup() {
         return "member/signup";
+    }
+
+    @PostMapping("/signup")
+    public String signup(@Valid MemberForm memberForm) {
+        memberService.signup(memberForm.getUsername(), memberForm.getPassword(), memberForm.getEmail(), memberForm.getNickname());
+
+        return "redirect:/member/login";
     }
 }
