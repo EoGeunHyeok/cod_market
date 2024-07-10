3ce46e90e38b978dba1adc76df661e91f4b72d04
EoGeunHyeok <eghsamo@naver.com>
2024-07-10 14:20:52 +0900

    detail data

--- src/test/java/com/cod/market/service/ProductServiceTests.java
+++ src/test/java/com/cod/market/service/ProductServiceTests.java
@@ -15,8 +15,8 @@ public class ProductServiceTests {
     @DisplayName("강의 제품 생성")
     void test1 () {
         for ( int i = 1; i <= 200; i++) {
-            String name = String.format("테스트 상품:[%03d]", i);
-            int price = 1;
+            String name = String.format("짱곰이의 플러팅 :[%03d]", i);
+            int price = 100;
             productService.create(name, price);
         }
     }
