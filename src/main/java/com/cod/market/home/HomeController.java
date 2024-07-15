8d32ac143792acddd62d54d810f88b4916e3ebdd
EoGeunHyeok <eghsamo@naver.com>
2024-07-15 15:09:39 +0900

    add admin poroduct content

--- src/main/java/com/cod/market/home/HomeController.java
+++ src/main/java/com/cod/market/home/HomeController.java
@@ -2,6 +2,7 @@ package com.cod.market.home;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -10,4 +11,6 @@ public class HomeController {
     public String index(){
         return "home/main";
     }
+
+
 }
