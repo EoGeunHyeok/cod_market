a4c7888269ef3342b8f7c7934cb024b398c57b00
EoGeunHyeok <eghsamo@naver.com>
2024-07-16 14:47:21 +0900

    signup password encode

--- src/main/java/com/cod/market/base/initData/DevIntiData.java
+++ src/main/java/com/cod/market/base/initData/DevIntiData.java
@@ -15,7 +15,7 @@ public class DevIntiData implements BeforeIntiData {
         return args -> {
             beforeInit();
 
-            String password = "{noop}1234"; // noop을 해주면 난수처럼 들어감
+            String password = "1234"; // noop을 해주면 난수처럼 들어감
             memberService.signup("user1",password,"user1@test.com","user1");
             memberService.signup("user2",password,"user2@test.com","user2");
             memberService.signup("cake",password,"cake@test.com","예지");
